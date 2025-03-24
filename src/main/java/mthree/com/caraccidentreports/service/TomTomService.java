0493d73a7954875b325d711d01c438e4a0759ac5
sydney <kimsydney18@gmail.com>
2025-03-24 17:19:03 -0400

    base url and getIncidents with restClient

--- src/main/java/mthree/com/caraccidentreports/service/TomTomService.java
+++ src/main/java/mthree/com/caraccidentreports/service/TomTomService.java
@@ -14,13 +14,21 @@ public class TomTomService {
 
     @Autowired
     private RestClient restClient;
+    // when running, paste API_KEY into this variable but don't push to github
+    private final String API_KEY = "";
+    private final String BASE_URL = "api.tomtom.com";
 
     public TomTomService(RestClient restClient) {
         this.restClient = restClient;
     }
 
     // input bounding box ?
-    public String getIncidents(String url) {
+    public String getIncidents(String bbox) {
+        // replace brackets
+        String url = BASE_URL + "/traffic/services/5/incidentDetails?bbox=" + bbox + "key=" + API_KEY +
+                "&fields={incidents{type,geometry{type,coordinates},properties{iconCategory}}}&categoryFilter=1&language=en-GB&t=1111&timeValidityFilter=pres";
+
         return "";
+        // return restClient.get().uri(url).retrieve().body(TrafficIncidentResponse.class);
     }
 }
