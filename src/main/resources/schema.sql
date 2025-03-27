2f8779e8c1c2aead373570ad33910712be11f235
Sizhon <zhu.simon.bla@gmail.com>
2025-03-27 10:43:53 -0400

    schema update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,7 @@ drop table if exists customer;
 create table customer (
     cid int AUTO_INCREMENT primary key,
     username varchar(255),
+    lid varchar(100),
     fName varchar(255) default null,
     lName varchar(255) default null
 );
@@ -11,13 +12,20 @@ create table user_cred (
     username varchar(25),
     password varchar(25),
     email varchar(255),
-    city VARCHAR(100)
 );
 
 drop table if exists incident;
 create table incident (
-    iid varchar(255) primary key,
-    from_street varchar(255),
-    to_street varchar(255),
-    incident_type varchar(255)
+    iid varchar(100) primary key,
+    lid varchar(100)
+    from_street varchar(100),
+    to_street varchar(100),
+    incident_type varchar(100),
 );
+
+drop table if exists location;
+create table location (
+    lid varchar(100) primary key,
+    state varchar(100),
+    city varchar(100)
+)
